80568592dabbad76cac996cd2267f556444722d4
Louise Darche <louise.darche@gmail.com>
2018-05-16 21:00:14 +0200

    missing the front

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "flats#index"
+  resources :flats
+  # , only: [:index, :show, :new, :create, :edit, :update]
+  # delete "flats/:id", to: "flat#destroy", as: :delete_flat
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name: 'Great artist\'s room in Montmartre',
+    address: '6 rue nicolet 75018 Paris',
+    description: 'An amazing view on Paris, with the proximity of la Place du Tertre for a vintage feel.',
+    price_per_night: 125,
+    number_of_guests: 2
+  },
+  {
+    name: 'Central flat in partyful Barcelona',
+    address: '8 carrer d\'en sants, Born, Barcelona',
+    description: 'The best neighborhood to discover the city, in a neat and cosy place. Great for families.',
+    price_per_night: 100,
+    number_of_guests: 5
+  },
+  {
+    name: 'Perfect flat for young lovers',
+    address: 'Ile de la cité, Paris',
+    description: 'A nice room beneath the Parisian roofs, for the most romantic stay of all.',
+    price_per_night: 250,
+    number_of_guests: 2
+  }
+]
+Flat.create!(flats_attributes)
+puts 'Finished!'
